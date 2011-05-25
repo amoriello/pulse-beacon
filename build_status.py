1d7399195ce324dc8db2e6cda693b51180cf1e6c
Amoriello Hutti <amoriello.hutti@gmail.com>
2011-05-25 22:51:06 +0200

    [+] Semantic action

--- build_status.py
+++ build_status.py
@@ -4,8 +4,8 @@ from xmlrpclib import ServerProxy, Error
 
 
 PULSE_URL      = "http://localhost:8080/xmlrpc"
-PULSE_USER     = "valblade"
-PULSE_PASSWORD = "valblade"
+PULSE_USER     = "USER"
+PULSE_PASSWORD = "PASSWORD"
 
 
 server = ServerProxy(PULSE_URL)
@@ -20,23 +20,24 @@ def log_on(server):
 
 def pool_project(token, project_name):
 	completeOnly = True
-	project = server.RemoteApi.getLatestBuildsForProject(token, project_name, completeOnly, 1)
-	curr_project_id =  project[0]['id']
+
+        next_build_id = server.RemoteApi.getNextBuildNumber(token, project_name)
+        curr_build_id = next_build_id - 1
 
 	while True:
 		time.sleep(2)
-		next_build_id = server.RemoteApi.getNextBuildNumber(token, "ltb")
-		
-		if (next_build_id != (curr_project_id + 1)):
-			curr_project_id = curr_project_id + 1
-			build = server.RemoteApi.getBuild(token, project_name, curr_project_id)
+		next_build_id = server.RemoteApi.getNextBuildNumber(token, project_name)
+
+		if (next_build_id != (curr_build_id + 1)):
+			curr_build_id = curr_build_id + 1
+			build = server.RemoteApi.getBuild(token, project_name, curr_build_id)
 
 			print build[0]['reason']
 
 			while (build[0]['status'] == "in progress"):
 				time.sleep(5)
 				print "Still building..."
-				build = server.RemoteApi.getBuild(token, project_name, curr_project_id)
+				build = server.RemoteApi.getBuild(token, project_name, curr_build_id)
 
 			print build[0]['status']
 
